10aab628e05f6ea4d01e3a9e3c9a1d01b3a4c9d3
Shujaa Gideon <shujaagideon@gmail.com>
2025-01-09 19:16:01 +0300

    switch paypal to live

--- convex/payments.ts
+++ convex/payments.ts
@@ -7,8 +7,8 @@ import { api } from "./_generated/api"
 import paypal from "@paypal/checkout-server-sdk";
 
 const environment = new paypal.core.LiveEnvironment(
-  process.env.PAYPAL_CLIENT_ID,
-  process.env.PAYPAL_CLIENT_SECRET
+  process.env.PAYPAL_CLIENT_ID!,
+  process.env.PAYPAL_CLIENT_SECRET!
 )
 const client = new paypal.core.PayPalHttpClient(environment)
 
